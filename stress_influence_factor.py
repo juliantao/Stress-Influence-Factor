da44502f4e351246faae7592f7520528ef149da4
JulianTao <julian.tao@asu.edu>
2022-01-18 10:51:49 -0700

    Refactor python script, better printout

--- stress_influence_factor.py
+++ stress_influence_factor.py
@@ -23,7 +23,6 @@
 # 2) hit run button and review the results
 #
 
-
 import math
 
 #### function I_B defines the Newark's solution for Boussinesq method and calculate the influence factor IB under the corner of a rectangle loading area
@@ -40,21 +39,15 @@ def I_B(B, L, zf):
     r1 = 2 * B * L * zf * math.sqrt(t1) / (t3 * t1 + t2)
     # second ratio
     r2 = (t1 + zf * zf) / t1
-
     if t1 < t2 / t3:
-
         I_b = 1 / 4 / math.pi * (r1 * r2 + math.pi - math.asin(r1))
-
     else:
-
         I_b = 1 / 4 / math.pi * (r1 * r2 + math.asin(r1))
-
         return I_b
 
 
 #### function I_Poulos defines Poulus approximation equation to calculate the influence factor IB under the center of a rectangle loading area
 def I_Poulos(B, L, zf):
-
     I_Poulos = 1 - (1 / (1 + (B / 2 / zf) ** (1.38 + 0.62 * B / L))) ** (
         2.60 - 0.84 * B / L
     )
@@ -68,42 +61,45 @@ def I_12(B, L, zf):
     return I_12
 
 
+#### define calculation and output function
+def calc_factors(B, L, zf):
+
+    IB_corner = I_B(B, L, zf)
+    IB_cquarter = I_B(B / 2, L / 2, zf)
+    IB_center = 4 * IB_cquarter
+    IP = I_Poulos(B, L, zf)
+    I12 = I_12(B, L, zf)
+
+    delta_BP = (IP - IB_center) / IB_center
+    delta_B12 = (I12 - IB_center) / IB_center
+    delta_P12 = (IP - I12) / IP
+
+    print(
+        "\n IB_corner is: ",
+        f"{IB_corner:.3}",
+        "\n IB_cquarter is: ",
+        f"{IB_cquarter:.3}",
+        "\n IB_center is: ",
+        f"{IB_center:.3}",
+        "\n I_Poulos at center is: ",
+        f"{IP:.3}",
+        "\n I_12 everywhere is: ",
+        f"{I12:.3}",
+        "\n delta_BP: ",
+        f"{delta_BP:.3%}",
+        "\n delta_B12: ",
+        f"{delta_B12:.3%}",
+        "\n delta_P12: ",
+        f"{delta_P12:.3%}",
+    )
+    return
+
+
 #############################################
 ###############Input###############
 #############################################
-B = 0.5
-L = 1.0
+B = 1.0
+L = 1.5
 zf = 1.2
 
-IB_corner = I_B(B, L, zf)
-IB_center = 4 * I_B(B / 2, L / 2, zf)
-I_Poulos = I_Poulos(B, L, zf)
-I12 = I_12(B, L, zf)
-
-delta_BP = (I_Poulos - IB_center) / IB_center
-delta_B12 = (I12 - IB_center) / IB_center
-delta_P12 = (I_Poulos - I12) / I_Poulos
-
-print(
-    "Under the corner, the  stress influence factor calculated using the Boussinesq method is: ",
-    IB_corner,
-)
-print(
-    "Under the center, the stress influence factor calculated using the Boussinesq method is: ",
-    IB_center,
-)
-print(
-    "Under the center, the stress influence factor calculated using the Poulus approximation method is: ",
-    I_Poulos,
-)
-print(
-    "Under the center, the  stress influence factor calculated using the 1:2 method is: ",
-    I12,
-)
-
-print(
-    "The difference percentages, delta_BP, delta_B12 and delta_P12 are ",
-    delta_BP,
-    delta_B12,
-    delta_P12,
-)
+calc_factors(B, L, zf)
