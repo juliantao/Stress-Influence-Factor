48e8826607571563f484ab4f74bdd4cabdb7c4ca
JulianTao <julian.tao@asu.edu>
2022-01-18 10:22:20 -0700

    Refactor python script

--- stress_influence_factor.py
+++ stress_influence_factor.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# This notebook presents the common calculation methods for stress increment induced by a rectangular loading.
+# This script presents the common calculation methods for stress increment induced by a rectangular loading.
 #
 # Defination of the problem:
 #
@@ -26,63 +26,30 @@
 
 import math
 
-#### function IB defines the Newark's solution for Boussinesq method and calculate the influence factor IB under the corner of a rectangle loading area
+#### function I_B defines the Newark's solution for Boussinesq method and calculate the influence factor IB under the corner of a rectangle loading area
 
 
-def IB(B, L, zf):
+def I_B(B, L, zf):
+    # first term
+    t1 = B ** 2 + L ** 2 + zf ** 2
+    # second term
+    t2 = B ** 2 * L ** 2
+    # third term
+    t3 = zf ** 2
+    # first ratio
+    r1 = 2 * B * L * zf * math.sqrt(t1) / (t3 * t1 + t2)
+    # second ratio
+    r2 = (t1 + zf * zf) / t1
 
-    if B ** 2 + L ** 2 + zf ** 2 < B ** 2 * L ** 2 / zf ** 2:
-        I = (
-            1
-            / 4
-            / math.pi
-            * (
-                (
-                    2
-                    * B
-                    * L
-                    * zf
-                    * (B ** 2 + L ** 2 + zf ** 2) ** 0.5
-                    / (zf ** 2 * (B ** 2 + L ** 2 + zf ** 2) + B ** 2 * L ** 2)
-                )
-                * ((B ** 2 + L ** 2 + 2 * zf ** 2) / (B ** 2 + L ** 2 + zf ** 2))
-                + math.pi
-                - math.asin(
-                    2
-                    * B
-                    * L
-                    * zf
-                    * (B ** 2 + L ** 2 + zf ** 2) ** 0.5
-                    / (zf ** 2 * (B ** 2 + L ** 2 + zf ** 2) + B ** 2 * L ** 2)
-                )
-            )
-        )
+    if t1 < t2 / t3:
+
+        I_b = 1 / 4 / math.pi * (r1 * r2 + math.pi - math.asin(r1))
+
     else:
-        I = (
-            1
-            / 4
-            / math.pi
-            * (
-                (
-                    2
-                    * B
-                    * L
-                    * zf
-                    * (B ** 2 + L ** 2 + zf ** 2) ** 0.5
-                    / (zf ** 2 * (B ** 2 + L ** 2 + zf ** 2) + B ** 2 * L ** 2)
-                )
-                * ((B ** 2 + L ** 2 + 2 * zf ** 2) / (B ** 2 + L ** 2 + zf ** 2))
-                + math.asin(
-                    2
-                    * B
-                    * L
-                    * zf
-                    * (B ** 2 + L ** 2 + zf ** 2) ** 0.5
-                    / (zf ** 2 * (B ** 2 + L ** 2 + zf ** 2) + B ** 2 * L ** 2)
-                )
-            )
-        )
-        return I
+
+        I_b = 1 / 4 / math.pi * (r1 * r2 + math.asin(r1))
+
+        return I_b
 
 
 #### function I_Poulos defines Poulus approximation equation to calculate the influence factor IB under the center of a rectangle loading area
@@ -94,27 +61,33 @@ def I_Poulos(B, L, zf):
     return I_Poulos
 
 
-#### function I12 defines the 1:2 method to calculate the influence factor IB
-def I12(B, L, zf):
+#### function I_12 defines the 1:2 method to calculate the influence factor IB
+def I_12(B, L, zf):
 
-    I12 = (B * L) / (B + zf) / (L + zf)
-    return I12
+    I_12 = (B * L) / (B + zf) / (L + zf)
+    return I_12
 
 
-# In[ ]:
-
-## input
+#############################################
+###############Input###############
+#############################################
 B = 0.5
 L = 1.0
 zf = 1.2
 
-
-IB_corner = IB(B, L, zf)
-I12 = I12(B, L, zf)
-IB_center = 4 * IB(B / 2, L / 2, zf)
+IB_corner = I_B(B, L, zf)
+IB_center = 4 * I_B(B / 2, L / 2, zf)
 I_Poulos = I_Poulos(B, L, zf)
+I12 = I_12(B, L, zf)
 
+delta_BP = (I_Poulos - IB_center) / IB_center
+delta_B12 = (I12 - IB_center) / IB_center
+delta_P12 = (I_Poulos - I12) / I_Poulos
 
+print(
+    "Under the corner, the  stress influence factor calculated using the Boussinesq method is: ",
+    IB_corner,
+)
 print(
     "Under the center, the stress influence factor calculated using the Boussinesq method is: ",
     IB_center,
@@ -127,10 +100,10 @@ print(
     "Under the center, the  stress influence factor calculated using the 1:2 method is: ",
     I12,
 )
+
 print(
-    "Under the corner, the  stress influence factor calculated using the Boussinesq method is: ",
-    IB_corner,
+    "The difference percentages, delta_BP, delta_B12 and delta_P12 are ",
+    delta_BP,
+    delta_B12,
+    delta_P12,
 )
-
-
-# In[ ]:
